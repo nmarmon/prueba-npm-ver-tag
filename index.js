556ec6ac049114590d347574347cbdb7532750c7
nmarmon <eslivion@gmail.com>
2016-12-05 20:42:45 +0100

    index.js borrado

--- index.js
+++
@@ -1,6 +0,0 @@
-//console.log("Esta es la versión 1.0.0 del proyecto")
-var lodash = require ('lodash');
-
-var output = lodash.without ([1, 2, 3], 1);
-console.log (output);
-console.log ("esto es un cambio un poco mas importante, es la version 1.1.0");
